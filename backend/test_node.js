ea053595ddcb0c19b46b0ec6e09b5dbe123ec779
dimchantha717-spec <dimchantha717@gmail.com>
2026-03-12 17:04:55 +0700

    Delete backend directory

--- backend/test_node.js
+++
@@ -1,8 +0,0 @@
-try {
-    const bcrypt = require('bcryptjs');
-    console.log('bcryptjs found successfully');
-} catch (e) {
-    console.error('Error finding bcryptjs:', e.message);
-    console.log('Current __dirname:', __dirname);
-    console.log('Module paths:', module.paths);
-}
