8137c9ec3ddc58405065f7ccbfc49beb10599eb0
dimchantha717-spec <dimchantha717@gmail.com>
2026-05-06 16:46:25 +0700

    Delete public directory

--- public/assets/constants-legacy-06db4406.js
+++
@@ -1,1 +0,0 @@
-System.register([],function(e,t){return{setters:[],execute:function(){e("D",["IT","Accounting","Marketing","HR","Operations","Sales","Logistics","Customer Service","Legal","Management"])}}});
