e581db1b07dbe59487e8f5564c1ba8172d41cd3d
dimchantha717-spec <dimchantha717@gmail.com>
2026-05-07 17:05:56 +0700

    Add files via upload

---
+++ public/assets/constants-legacy-06db4406.js
@@ -0,0 +1,1 @@
+System.register([],function(e,t){return{setters:[],execute:function(){e("D",["IT","Accounting","Marketing","HR","Operations","Sales","Logistics","Customer Service","Legal","Management"])}}});
